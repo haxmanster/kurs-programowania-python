36f801f9ff0fa472cf9aade8613b341bca04d9cc
haxmanster <grzegorz-wolyniec@o2.pl>
2018-03-22 21:55:19 +0100

    I added the interval +180 and - 180 degrees
    Change name function NolrmalizacjaKata  to normalizacja_kata

--- zegar_v1.py
+++ zegar_v1.py
@@ -8,13 +8,15 @@ print("|////////////////////////////////////////////////////////////////////////
 print ("Witaj w moim programie do obliczania kata ktory tworza wskazowki zegara na tarczy ")
 
 
-def NormalizacjaKata(godzina, minuta):
-  if a - b < 0:
-    return -c
-  if a - b >= 180:
-    return 360 - c
-  else:
-    return c
+def normalizacja_kata(godzina, minuta):
+  if suma_katow > 0:
+    if suma_katow < 0:
+     return suma_katow
+  if suma_katow < -180:
+     return suma_katow + 360
+  if suma_katow > 180:
+     return suma_katow
+  return -suma_katow
 
 wybor = "t"
 t = str(wybor)
@@ -22,11 +24,12 @@ while wybor == "t":
   godzina = float(input("Podaj godzine "))
   minuta = float(input("Podaj minute "))
   stala = float(5.5)
-  a = stala * minuta
-  b = (30 * godzina)
-  c = a - b
+  kat_minuty = stala * minuta
+  kat_godziny = (30 * godzina)
+  suma_katow = kat_minuty - kat_godziny
 
-  print("Kąt pomiedzy wskazówkami zegara wynosi : ", NormalizacjaKata(godzina,minuta))
+
+  print("Kąt pomiedzy wskazówkami zegara wynosi : ", normalizacja_kata(godzina,minuta))
   wybor = str(input("Jeszcze raz (t/n)? "))
 print ("bye")
 
